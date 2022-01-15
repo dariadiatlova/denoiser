607ffdda0f87afe5da02606b86b3058ac8315084
diatlova <dari.diatlova@gmail.com>
2022-01-15 22:09:33 +0300

    config for train/val jsons

--- make_debug.sh
+++ make_debug.sh
@@ -10,5 +10,10 @@ path=egs/debug/tr
 if [[ ! -e $path ]]; then
     mkdir -p $path
 fi
-python3 -m denoiser.audio dataset/russian_debug/noisy > $path/noisy.json
-python3 -m denoiser.audio dataset/russian_debug/clean > $path/clean.json
+python3 -m denoiser.audio /home/dadyatlova_1/russian_speech_denoiser/DNS-Challenge/datasets/training_set_jan14_min35_10_40h/noisy_train > $path/noisy_train.json
+python3 -m denoiser.audio /home/dadyatlova_1/russian_speech_denoiser/DNS-Challenge/datasets/training_set_jan14_min35_10_40h/noisy_val > $path/noisy_val.json
+python3 -m denoiser.audio /home/dadyatlova_1/russian_speech_denoiser/DNS-Challenge/datasets/training_set_jan14_min35_10_40h/noisy_test > $path/noisy_test.json
+
+python3 -m denoiser.audio /home/dadyatlova_1/russian_speech_denoiser/DNS-Challenge/datasets/training_set_jan14_min35_10_40h/clean_train > $path/clean_train.json
+python3 -m denoiser.audio /home/dadyatlova_1/russian_speech_denoiser/DNS-Challenge/datasets/training_set_jan14_min35_10_40h/clean_val > $path/clean_val.json
+python3 -m denoiser.audio /home/dadyatlova_1/russian_speech_denoiser/DNS-Challenge/datasets/training_set_jan14_min35_10_40h/clean_test > $path/clean_test.json
