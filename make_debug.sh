617fec90243a352fac97fca1e0df35c914bee58b
diatlova <dari.diatlova@gmail.com>
2022-04-17 18:07:53 +0300

    upd shell script

--- make_debug.sh
+++ make_debug.sh
@@ -6,14 +6,14 @@
 # LICENSE file in the root directory of this source tree.
 # authors: adiyoss and adefossez
 
-path=egs/debug/jan14_min35_10_40
+path=egs/debug/april_100
 if [[ ! -e $path ]]; then
     mkdir -p $path
 fi
-python3 -m denoiser.audio /home/dadyatlova_1/russian_speech_denoiser/DNS-Challenge/datasets/training_set_jan14_min35_10_40h/noisy_train > $path/noisy_train.json
-python3 -m denoiser.audio /home/dadyatlova_1/russian_speech_denoiser/DNS-Challenge/datasets/training_set_jan14_min35_10_40h/noisy_val > $path/noisy_val.json
-python3 -m denoiser.audio /home/dadyatlova_1/russian_speech_denoiser/DNS-Challenge/datasets/training_set_jan14_min35_10_40h/noisy_test > $path/noisy_test.json
+python3 -m denoiser.audio /home/dadyatlova_1/dataset/main/data_100_hours/noisy_train > $path/noisy_train.json
+python3 -m denoiser.audio /home/dadyatlova_1/dataset/main/data_100_hours/noisy_val > $path/noisy_val.json
+python3 -m denoiser.audio /home/dadyatlova_1/dataset/main/data_100_hours/noisy_test > $path/noisy_test.json
 
-python3 -m denoiser.audio /home/dadyatlova_1/russian_speech_denoiser/DNS-Challenge/datasets/training_set_jan14_min35_10_40h/clean_train > $path/clean_train.json
-python3 -m denoiser.audio /home/dadyatlova_1/russian_speech_denoiser/DNS-Challenge/datasets/training_set_jan14_min35_10_40h/clean_val > $path/clean_val.json
-python3 -m denoiser.audio /home/dadyatlova_1/russian_speech_denoiser/DNS-Challenge/datasets/training_set_jan14_min35_10_40h/clean_test > $path/clean_test.json
+python3 -m denoiser.audio /home/dadyatlova_1/dataset/main/data_100_hours/clean_train > $path/clean_train.json
+python3 -m denoiser.audio /home/dadyatlova_1/dataset/main/data_100_hours/clean_val > $path/clean_val.json
+python3 -m denoiser.audio /home/dadyatlova_1/dataset/main/data_100_hours/clean_test > $path/clean_test.json
